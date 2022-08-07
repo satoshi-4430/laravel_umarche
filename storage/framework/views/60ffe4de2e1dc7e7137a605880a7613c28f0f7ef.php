9c06d2dcdee1d5be85da1b50ff160016e5d4f6c5
yoshizawa <44303104sa@gmail.com>
2022-08-08 01:44:28 +0900

    image detete

--- storage/framework/views/60ffe4de2e1dc7e7137a605880a7613c28f0f7ef.php
+++ storage/framework/views/60ffe4de2e1dc7e7137a605880a7613c28f0f7ef.php
@@ -12,7 +12,7 @@ if(session('status') === 'info'){ $bgColor = 'bg-blue-300';}
 if(session('status') === 'alert'){$bgColor = 'bg-red-500';} 
 ?> 
 <?php if(session('message')): ?> 
-  <div class="<?php echo e($bgColor); ?> w-1/2 mx-auto p-2 text-white"> 
+  <div class="<?php echo e($bgColor); ?> w-1/2 mx-auto p-2 my-4 text-white"> 
   <?php echo e(session('message')); ?> 
   </div> 
 <?php endif; ?><?php /**PATH C:\xampp\htdocs\laravel\umarche\resources\views/components/flash-message.blade.php ENDPATH**/ ?>
