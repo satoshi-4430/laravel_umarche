2e901647fcb82155d9c50cc60ff9ac3a7f2249b0
yoshizawa <44303104sa@gmail.com>
2022-08-11 13:41:38 +0900

    show

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -9,6 +9,11 @@ use Illuminate\Support\Facades\DB;
 
 class ItemController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:users');
+    }
+
     public function index()
     {
         $stocks = DB::table('t_stocks') 
@@ -40,6 +45,13 @@ class ItemController extends Controller
         // $products = Product::all();
 
         return view('user.index', compact('products'));
+    }
+
+    public function show($id)
+    {
+        $product =Product::findOrFail($id);
+
+        return view('user.show', compact('product'));
     }
     
 }
